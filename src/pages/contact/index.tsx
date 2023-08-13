b4d07139aaef2a498a618497b8c0e761ccf59508
Shkirenko <shkirenkoroma@mail.ru>
2023-08-13 14:27:08 +0300

    feature/I18NEXT: added several features

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -6,7 +6,8 @@ const Contact: FC= ():JSX.Element => {
 const {t} = useTranslation()
 
     return <div className="alert alert-success" role="alert">
-            <h1>{t("parag.contact")}</h1> </div>
+            <h1>{t("parag.contact")}</h1> 
+            </div>
     
 }
 
