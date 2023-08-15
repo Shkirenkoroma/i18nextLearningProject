31cc3d1b0bb8069e8e46b576209950fb3b3dd0dd
Shkirenko <shkirenkoroma@mail.ru>
2023-08-15 22:26:52 +0300

    fix: I18NEXT: fixed code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
 import { FC } from 'react';
-import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/home';
 import Contact from './pages/contact';
 import NavBar from './components/navbar';
-//необходимо подключить bootstrap в index.html  public
-//необходимо подключить react route dom
-//необходимо подключить select bиз bootstrap
-//необходимо подключить i18next
-//скопировать шаблон с официальной страницы i18next
-//необходимо устаовить зависимость i18 next http backend
-
+import './App.css';
 
 const App:FC = ():JSX.Element => {
   return (
